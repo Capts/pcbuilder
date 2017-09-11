ffa45a584e2e23c9fedc25dd2d5cfbf41331bd24
Capts <kevin.572.94@gmail.com>
2017-09-12 00:04:56 +0800

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @include('partials.head')
+</head>
+<body>
+    <div id="app">
+       @include('partials.nav')
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
